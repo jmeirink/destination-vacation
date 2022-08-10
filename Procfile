2427c7d068fc6a5c43e6943fbc0787b7dbb44349
Jesse Meirink <jesse@meirink.com>
2022-08-10 10:43:20 -0500

    Config deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn destinationvacation.wsgi
--- destinationvacation/settings.py
+++ destinationvacation/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#mtgdonh$*9v4(2acr6n^&emd-6p09gqjb3a1dyl22r94jf0o3'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG') == 'True'
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = []
 
@@ -132,3 +136,9 @@ LOGOUT_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
